c40a8f4f1a064e0f92795f61009a2a6e2a08d9bf
depazjose <jose.depaz@gmail.com>
2019-11-10 00:01:26 -0500

    deploy to heroku

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,6 @@ if (process.env.NODE_ENV === "dev") {
     urlDb = "mongodb://localhost:27017/cafe";
     console.log("Conectado a dev local");
 } else {
-    urlDb = "mongodb+srv://kerberos:<>@clusterjdepaz-9wwdu.gcp.mongodb.net/cafe?retryWrites=true&w=majority";
+    urlDb = "mongodb+srv://kerberos:as10469j41@clusterjdepaz-9wwdu.gcp.mongodb.net/cafe?retryWrites=true&w=majority";
 }
 process.env.URL_DATABASE = urlDb;
